fd3a7db0ce8b8c4b24f0bc73e575908f9cd14532
chvainickas <38086543+chvainickas@users.noreply.github.com>
2023-01-06 21:45:03 +0000

    continuing working with script

--- MathsGame/script.js
+++ MathsGame/script.js
@@ -1,20 +1,40 @@
+var playing = false;
+var score;
+var action;
+var timeRemaining;
 
-var score =0;
-
-if document.getElementById("startreset:active"). {
-
-}
-// if we click on the start/reset
+document.getElementById("startreset").onclick = function() {
     //  if we are playing
+    if (playing) {
         //reload
-    // if we're not playing
-        //set score to zero
+        location.reload();
+         }
+        else{
+            hide("gameover");
+            playing=true;
+            //set score to zero
+        score = 0;
+        document.getElementById("gamescore").innerHTML = score;
         //show countdown box
+        show("timer");
+        timeRemaining = 2;
+        document.getElementById("time").innerHTML = timeRemaining;
+        // change button to reset
+        document.getElementById("startreset").innerHTML = "Reset"
+        // reduce time by 1 sec
+        startCountdown();
+        }
+}
         // reduce time by 1 sec
             //timeleft
                 //yes-> cnt
                 //no-> gameover
                     // show score
+// if we click on the start/reset
+    //  if we are playing
+        
+    // if we're not playing
+
         // change button to reset
         // generate new q&a
 
@@ -26,4 +46,38 @@ if document.getElementById("startreset:active"). {
                 // show correct box for 1s
                 //generate new q&a
             //no
-                //show try again box for 1 sec
+                //show try again box for 1 s
+                //start counter
+function startCountdown(params) {
+
+    action = setInterval(() => {
+        timeRemaining -= 1;
+        document.getElementById("time").innerHTML = timeRemaining;
+        if (timeRemaining == 0) {//game over
+            clearInterval(action);
+            playing=false;
+            hide("timer");
+            hide("correct")
+            hide("wrong")
+            document.getElementById("gameover").innerHTML 
+            = "<p>Game Over</p><p>Your score is "+ score +".</p>";
+            document.getElementById("startreset").innerHTML = "Start Game!";
+            show("gameover") = "block";
+            
+            
+            
+        }
+    }, 1000);
+}
+function hide(Id) {
+    document.getElementById(Id).style.display = "none";
+    
+}
+function show(Id) {
+    document.getElementById(Id).style.display = "block";
+    
+}
+function generateQA() {
+
+    
+}
