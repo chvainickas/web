040318a694bf9c41ffd2dbf82fb6f7fa9e0e11d3
chvainickas <38086543+chvainickas@users.noreply.github.com>
2023-01-06 03:02:30 +0000

    finished styling and started the script

---
+++ MathsGame/script.js
@@ -0,0 +1,29 @@
+
+var score =0;
+
+if document.getElementById("startreset:active"). {
+
+}
+// if we click on the start/reset
+    //  if we are playing
+        //reload
+    // if we're not playing
+        //set score to zero
+        //show countdown box
+        // reduce time by 1 sec
+            //timeleft
+                //yes-> cnt
+                //no-> gameover
+                    // show score
+        // change button to reset
+        // generate new q&a
+
+// if we click on answer box
+    //if we are playing 
+        //correnct
+            //yes
+                // increase score by 1
+                // show correct box for 1s
+                //generate new q&a
+            //no
+                //show try again box for 1 sec
